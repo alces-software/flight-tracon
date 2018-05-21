4c1fcfe27b0969c5dc343dbb10889ad01ac72a89
Ben Armston <ben.armston@alces-software.com>
2018-05-21 14:47:40 +0100

    FlyConfig is a simple data record

--- lib/tracon/fly_config.rb
+++ lib/tracon/fly_config.rb
@@ -1,33 +1,26 @@
 module Tracon
   class FlyConfig
-    attr_accessor :template_set
-    attr_accessor :key_pair
-    attr_accessor :region
+
     attr_accessor :access_key
-    attr_accessor :secret_key
+    attr_accessor :cluster_name
     attr_accessor :domain
-    
+    attr_accessor :key_pair
+    attr_accessor :qualified_cluster_name
+    attr_accessor :queue_name
+    attr_accessor :region
+    attr_accessor :secret_key
+    attr_accessor :template_set
+
     def initialize(cluster, queue)
-      @cluster = cluster
       @domain = cluster.domain
-      @queue = queue
       @access_key = ENV['AWS_ACCESS_KEY_ID']
       @secret_key = ENV['AWS_SECRET_ACCESS_KEY']
       @template_set = ENV['FLY_TEMPLATE_SET']
       @key_pair = ENV['FLY_KEY_PAIR']
       @region = Thread.current[:aws_region] || 'eu-west-1'
-    end
-
-    def queue_name
-      @queue.name
-    end
-
-    def cluster_name
-      @cluster.name
-    end
-
-    def qualified_cluster_name
-      @cluster.qualified_name
+      @queue_name = queue.name
+      @cluster_name = cluster.name
+      @qualified_cluster_name = cluster.qualified_name
     end
   end
 end
